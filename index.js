e5e30c992db3dffaf2d69100ef161ad7800ee39a
noora20FH <noora20fairy@gmail.com>
2023-02-17 13:44:28 +0700

    note

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const linkHome = document.getElementById("link-home");
 const linkAbout = document.getElementById("link-about");
 const root = document.getElementById("root");
 
+//event heandler. link yg diklik akan menuju link yg dituju namun tidak meminta request dari server
 linkHome.onclick = function (event) {
   event.preventDefault();
-  history.pushState(null, "", event.target.href);
+  history.pushState(null, "", event.target.href);//target.href akan mengambil href dari id link-home di index.html
   root.textContent = "Welcome to Home Page";
 };
 
 linkAbout.onclick = function (event) {
   event.preventDefault();
-  history.pushState(null, "", event.target.href);
+  history.pushState(null, "", event.target.href);//target.href akan mengambil href dari id link-about di index.html
   root.textContent = "Welcome to About Page";
 };
 
